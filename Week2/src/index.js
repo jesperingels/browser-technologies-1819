2d51e3f4034a4ce9f71e8dd87e20607703998625
jesperingels <jesper.ingels@gmail.com>
2019-03-18 16:44:12 +0100

    body-parser werkt

--- Week2/src/index.js
+++ Week2/src/index.js
@@ -1,8 +1,39 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+// const app = express();
+const PORT = 3030;
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
-});
+express()
+    .set('view engine', 'ejs')
+    .set('views', 'views')
+    .use(express.static('public'))
+    .use(bodyParser.urlencoded({extended: true}))
+    .get('/', home)
+    .post('/', form)
+    .listen(PORT, console.log('listening on port: ' + PORT));
 
-app.listen(3001);
+function home(req, res) {
+    res.render('pages/index.ejs');
+    console.log('served index.ejs from pages')
+}
+
+function form(req, res) {
+    console.log(req.body);
+    res.send('<h1>form sent</h1>')
+}
+
+
+// app.set('view engine', 'ejs');
+//
+// app.use(express.static('public'));
+// app.use(express.bodyParser());
+//
+// app.get('/', function (req, res) {
+//     res.render('pages/index');
+// });
+//
+// app.post('/data', function (req, res) {
+//     console.log(req.body);
+// });
+//
+// app.listen(port, () => console.log(`App listening on port ${port}!`));

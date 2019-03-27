d21e8b6d572f2c7c2d890e790eb337d4717e4ba4
jesperingels <jesper.ingels@gmail.com>
2019-03-27 11:10:33 +0100

    update app js

--- Week2/public/js/app.js
+++ Week2/public/js/app.js
@@ -4,7 +4,6 @@ const form2 = document.getElementById('form2');
 const form3 = document.getElementById('form3');
 const input = document.querySelectorAll('input');
 const progress = document.querySelector('progress');
-const inputValue = 0;
 
 forms.forEach(prop => {
     prop.style.display = 'flex';
@@ -12,14 +11,17 @@ forms.forEach(prop => {
 
 document.getElementById('nojs-form').style.display = 'none';
 
+// Submit all forms on click
 document.getElementById('send-form').addEventListener('click', ()=>{
     form1.submit();
     form2.submit();
     form3.submit();
 });
 
-const userInputs = document.querySelectorAll('input[name]')
+const userInputs = document.querySelectorAll('input[name]');
+const userSelect = document.querySelector("select");
 
+// Increase
 userInputs.forEach(function(el){
     el.addEventListener('change',function () {
         if(this.value === '' || this.value === null) {
@@ -30,9 +32,17 @@ userInputs.forEach(function(el){
         progress.value = document.querySelectorAll(".done").length*10;
         console.log(document.querySelectorAll(".done").length,this.value)
     })
-})
+});
 
-
+userSelect.addEventListener('change',function () {
+    if(this.value === '' || this.value === null) {
+        this.classList.remove("done")
+    } else {
+        this.classList.add("done")
+    }
+    progress.value = document.querySelectorAll(".done").length*10;
+    console.log(document.querySelectorAll(".done").length,this.value)
+});
 
 
 

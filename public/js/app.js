6cd09821b3a0c9fdf2918cf154af31b76e29d74b
jesperingels <jesper.ingels@gmail.com>
2019-03-27 14:38:33 +0100

    Update index.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,29 +1,68 @@
-var forms = document.querySelectorAll('.js-form');
-var form1 = document.getElementById('form1');
-var form2 = document.getElementById('form2');
-var form3 = document.getElementById('form3');
-var input = document.querySelectorAll('input');
-var progress = document.querySelector('progress');
+if(document.getElementById('nojs-form')) {
+    removeform();
+}
 
-forms.forEach(function (prop) {
-    prop.style.display = 'flex';
-});
+function removeform() {
+    // Removes an element from the document
+    var el = document.getElementById('nojs-form');
+    el.parentNode.removeChild(el);
+}
 
-document.getElementById('nojs-form').style.display = 'none';
+if(document.querySelector('main') && !document.querySelector('main.overview')) {
+    document.querySelector('main').insertAdjacentHTML('beforeend', '  <section class="js-form">\n' +
+        '        <progress value="0" max="100"></progress>\n' +
+        '        <form action="/all-data" method="post" id="form1">\n' +
+        '            <input name="name" type="text" placeholder="Name..."/>\n' +
+        '            <input name="date" type="date" placeholder="dd-mm-jjjj"/>\n' +
+        '            <input name="colour" type="text" placeholder="Favorite colour..."/>\n' +
+        '            <input name="animal" type="text" placeholder="Favorite animal..."/>\n' +
+        '            <input name="movie" type="text" placeholder="Favorite movie..."/>\n' +
+        '            <input name="education" type="text" placeholder="education..."/>\n' +
+        '            <input name="drink" type="text" placeholder="Favorite drink..."/>\n' +
+        '            <label>Favorite season:</label>\n' +
+        '            <select name="season">\n' +
+        '                <option value="summer">Summer</option>\n' +
+        '                <option value="winter">Winter</option>\n' +
+        '                <option value="spring">Spring</option>\n' +
+        '                <option value="autumn">Autumn</option>\n' +
+        '            </select>\n' +
+        '            <input name="age" type="text" placeholder="Your age..."/>\n' +
+        '        </form>\n' +
+        '        <button id="send-form">Verstuur</button>\n' +
+        '    </section>');
 
-// Submit all forms on click
-document.getElementById('send-form').addEventListener('click', function () {
-    form1.submit();
-    form2.submit();
-    form3.submit();
-});
 
-var userInputs = document.querySelectorAll('input[name]');
-var userSelect = document.querySelector("select");
+}
 
-// Increase
-userInputs.forEach(function(el){
-    el.addEventListener('change',function () {
+if(document.getElementById('send-form')) {
+    document.getElementById('send-form').addEventListener('click', function () {
+        form1.submit();
+    });
+
+    var userInputs = document.querySelectorAll('input[name]');
+    var userSelect = document.querySelector("select");
+
+    // Increase
+    userInputs.forEach(function(el){
+        el.addEventListener('change',function () {
+            if(this.value === '' || this.value === null) {
+                this.className = this.className.replace(/\bdone\b/g, "");
+            } else {
+                var name, arr;
+                name = "done";
+                arr = this.className.split(" ");
+                if (arr.indexOf(name) === -1) {
+                    this.className += " " + name;
+                }
+            }
+            progress.value = document.querySelectorAll(".done").length*10 + 20;
+            console.log(document.querySelectorAll(".done").length,this.value)
+        })
+    });
+
+    console.log(userInputs);
+
+    userSelect.addEventListener('change',function () {
         if(this.value === '' || this.value === null) {
             this.className.remove("done")
         } else {
@@ -31,18 +70,19 @@ userInputs.forEach(function(el){
         }
         progress.value = document.querySelectorAll(".done").length*10;
         console.log(document.querySelectorAll(".done").length,this.value)
-    })
-});
+    });
+}
 
-userSelect.addEventListener('change',function () {
-    if(this.value === '' || this.value === null) {
-        this.className.remove("done")
-    } else {
-        this.className.add("done")
-    }
-    progress.value = document.querySelectorAll(".done").length*10;
-    console.log(document.querySelectorAll(".done").length,this.value)
-});
+
+var forms = document.querySelectorAll('.js-form');
+var form1 = document.getElementById('form1');
+// var form2 = document.getElementById('form2');
+// var form3 = document.getElementById('form3');
+var input = document.querySelectorAll('input');
+var progress = document.querySelector('progress');
+
+
+
 
 
 
--- src/index.js
+++ src/index.js
@@ -33,11 +33,17 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use((req, res, next) => {
+    res.setHeader('Cache-Control', 'max-age=' + 365 * 24 * 60 *
+        60);
+    next();
+});
 
 app.get('/', home);
 app.post('/vraag2', quest1);
 app.post('/vraag3', quest2 );
 app.post('/data', data);
+app.post('/all-data', jsSubmit);
 app.get('/overview', overview);
 
 app.listen(port, console.log(`listening on port: ${port}`));
@@ -133,17 +139,46 @@ function overview(req, res) {
     fs.readFile('public/db/db.txt', 'utf8', function (err, db) {
         const obj = JSON.parse(db);
 
-        // for(let i = 0; i < obj.res.length; i++) {
-        //         //     console.log(obj.res[i].question1.name);
-        //         // }
-
         console.log(obj.res[1]);
-         // console.log(obj.res[0].question1.name);
-
         res.render('pages/overview.ejs', {
             data: obj.res
          })
 
     });
+}
+
+function jsSubmit (req, res) {
+    fs.readFile('public/db/db.txt', 'utf8', function readFileCallback(err, db){
+        if (err){
+            console.log(err);
+        } else {
+            const obj = JSON.parse(db); //now it an object
+            const data = {
+                question1: {
+                    name: req.body.name,
+                    date: req.body.date,
+                    colour: req.body.colour
+                },
+                question2: {
+                    animal: req.body.animal,
+                    movie: req.body.movie,
+                    education: req.body.education
+                },
+                question3: {
+                    drink: req.body.drink,
+                    season: req.body.season,
+                    age: req.body.age
+                }
+            };
+            obj.res.push(data);
+            const json = JSON.stringify(obj); //convert it back to json
+            fs.writeFile('public/db/db.txt', json, 'utf8', function (err) {
+                if (err) console.log(err);
+                console.log("Successfully Written to File.");
+            }); // write it back
+        }});
+
+
+    res.render('pages/sent.ejs');
 }
 
